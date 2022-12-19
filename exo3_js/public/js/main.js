919022061a9fb9d7f0dac18e3da3600fcb55f549
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-19 11:42:27 +0100

    fin

---
+++ exo3_js/public/js/main.js
@@ -0,0 +1,15 @@
+let premierChiffre;
+premierChiffre = prompt(30);
+let DeuxChiffre;
+DeuxChiffre = prompt(26);
+
+let plus;
+plus = `+`;
+
+let moins;
+moins =`-`;
+
+let divisé;
+divisé = `/`;
+
+alert(`${premierChiffre}${moins}${DeuxChiffre} = ${premierChiffre-DeuxChiffre} et ${premierChiffre}${plus}${DeuxChiffre} = ${premierChiffre+DeuxChiffre}${premierChiffre}${divisé}${DeuxChiffre} = ${premierChiffre/DeuxChiffre}`)
